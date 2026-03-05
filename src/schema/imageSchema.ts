30156097c7d16e27e73590da042296acc9dc35db
Ganes769 <ganeshsnawali@gmail.com>
2026-03-05 22:37:08 +0000

    added

--- src/schema/imageSchema.ts
+++ src/schema/imageSchema.ts
@@ -6,6 +6,7 @@ import {
   timestamp,
   bigint,
   uniqueIndex,
+  varchar,
 } from "drizzle-orm/pg-core";
 import { users } from "./userSchema";
 
@@ -19,7 +20,7 @@ export const images = pgTable(
     fileName: text("file_name"),
     mimeType: text("mime_type").notNull(),
     sizeBytes: bigint("size_bytes", { mode: "number" }).notNull(),
-  publicId: varchar("public_id", { length: 255 }).notNull()
+    publicId: varchar("public_id", { length: 255 }).notNull(),
     width: integer("width"),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     height: integer("height"),
